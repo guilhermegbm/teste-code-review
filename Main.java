97ff5cc3b2c58052793be0d244e18ab2f4318bf3
Guilherme Barboza Mendonça <guilhermebmendonca60@gmail.com>
2023-06-02 14:39:31 -0300

    Reset alterações

--- Main.java
+++ Main.java
@@ -5,11 +5,5 @@ import java.util.Date;
 class Main {
   public static void main(String[] args) {
     System.out.println("Teste");
-
-    Calendar c = Calendar.getInstance();
-
-    Date d = c.getTime();
-
-    System.out.println(new SimpleDateFormat("dd/MM/yyyy").format(d));
   }
 }
