e1a5419b37ec1b2f9b9acb7518184abdb2667843
Guilherme Barboza Mendonça <guilhermebmendonca60@gmail.com>
2023-06-02 15:35:20 -0300

    Print da Data Atual

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Teste");
+
+    Calendar c = Calendar.getInstance();
+
+    System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(c.getTime()));
   }
 }
